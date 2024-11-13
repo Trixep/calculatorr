656b01db3065bbded0c76ed5ca985860b1bcfafb
Trixep11 <82161045+Trixep@users.noreply.github.com>
2024-11-13 09:36:59 +0100

    Initial commit

---
+++ calc.js
@@ -0,0 +1,74 @@
+calculator = document.getElementById('calc-container');
+prevDisplay = document.getElementById('operation-display').innerHTML;
+display = document.getElementById('display').innerHTML;
+var currentNumber = "0";
+var currentCalculate[] = null;
+
+openCalc = () => {
+    calculator.style.visibility = 'visible';
+}
+
+closeCalc = () =>{
+    calculator.style.visibility = 'hidden';
+}
+
+function ButtonNumber(number){
+    currentNumber += number;
+    ChangeCurrentNumber();
+    console.log(currentNumber);
+}
+
+function Reset(){
+    currentNumber = "0";
+    document.getElementById('display').innerHTML = currentNumber;
+}
+
+function ResetAll(){
+    prevDisplay = "";
+    currentNumber = "0";
+    document.getElementById('display').innerHTML = currentNumber;
+}
+
+function Pow(){
+
+}
+
+function Devide(){
+
+}
+
+function Multiply(){
+
+}
+
+function Substract(){
+
+}
+
+function Add(){
+
+}
+
+function ChangeShit(){
+
+}
+
+function Float(){
+    
+}
+
+function Calculate(){
+
+}
+
+function ChangeCurrentNumber(){
+
+    if( currentNumber.charAt( 0 ) === '0' )
+        currentNumber = currentNumber.slice( 1 );
+
+    document.getElementById('display').innerHTML = currentNumber;
+}
+
+function ChangeCurrentCalculate(action){
+    currentCalculate
+}
